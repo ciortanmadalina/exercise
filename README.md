cfbb6ba078a7812fc9d1fa67c48ce9d882cd1461
Madalina Ciortan <madalina.ciortan@kiwi.com>
2024-05-01 22:10:59 +0200

    updated notebooks

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
 ## Problem Description
 
-The problem we are trying to solve is performing predictive maintenance on timeseries data. Predictive maintenance involves analyzing historical data to predict when a machine or equipment is likely to fail, allowing for proactive maintenance to be performed.
+The problem we are trying to solve is performing predictive maintenance on multivariate timeseries data. 
+Predictive maintenance involves analyzing historical data to predict when a machine or equipment is likely to fail, allowing for proactive maintenance to be performed.
 
 ## Solution Overview
+- It is valuable to predict failure as soon as possible
+- Maintenance always follows failure, for simplification it is discarded 
+- Goal: predict transition normal -> broken
 
-Our solution for performing predictive maintenance on timeseries data involves the following steps:
+### Two approaches 
+1. Classification: n timesteps before failure= 1, normal = 0 
+- XGBoost classification (see xgboost.ipynb)
+- Neural networks (see lstm.ipynb)
 
-1. Data Collection: We collect timeseries data from various sensors and monitoring devices installed on the machines or equipment.
+2. Regression: time to failure (see lstm.ipynb)
+- XGBoost regression (see xgboost.ipynb)
 
-2. Data Preprocessing: The collected data is preprocessed to handle missing values, outliers, and noise. This step may also involve data normalization or scaling.
-
-3. Feature Engineering: We extract relevant features from the timeseries data that can be used to train predictive models. This may include statistical features, frequency domain features, or time-domain features.
-
-4. Model Training: We train machine learning models using the preprocessed and engineered features. Popular models for timeseries data include recurrent neural networks (RNNs), long short-term memory (LSTM) networks, or gradient boosting machines (GBMs).
-
-5. Model Evaluation: We evaluate the trained models using appropriate evaluation metrics such as accuracy, precision, recall, or area under the receiver operating characteristic curve (AUC-ROC).
-
-6. Predictive Maintenance: Once the models are trained and evaluated, we can use them to predict when a machine or equipment is likely to fail. This allows us to schedule proactive maintenance activities, reducing downtime and improving operational efficiency.
 
 ## Getting Started
 
 To get started with our solution, follow these steps:
 
-1. Clone the repository: `git clone https://github.com/your-repo.git`
+1. Clone the repository: `https://github.com/ciortanmadalina/exercise.git`
 
 2. Install the required dependencies: `pip install -r requirements.txt`
 
-3. Run the data collection script: `python data_collection.py`
+3. Run the relevant noteboos: xgboost.ipynb, lstm.ipynb
+
+
+### Next steps
+- Perform cross validation
+- Train/test splits alternatives
+- Benchmark the NN models
+- Use transformer models
+- Unsupervised anomaly detection
+- Root cause analysis
--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
+
+from collections import Counter
+import random
+import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from collections import Counter
+from umap import UMAP
+from xgboost import XGBClassifier
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, precision_score, balanced_accuracy_score, confusion_matrix, f1_score
 import random
 import json
+from tqdm.notebook import tqdm
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import classification_report
 
 
 def preprocess(df):
@@ -26,3 +38,104 @@ def preprocess(df):
     df['idx'] = np.arange(df.shape[0])
     df['y'] = df["machine_status"].map(meta["target"])
     return df, cols
+
+
+
+def classification(X, y_classif, random_state=1):
+    idx_train, idx_test, Y_train, Y_test = train_test_split(np.arange(len(y_classif)), 
+    y_classif, test_size=0.3, random_state=random_state, stratify=y_classif)
+
+
+    c0_idx_train = idx_train[np.where(Y_train == 1)[0]]
+    c1_idx_train = idx_train[random.sample(list(np.where(Y_train == 0)[0]), int(len(c0_idx_train) *1.5))]
+
+
+    idx_train = np.concatenate([c0_idx_train, c1_idx_train])
+
+
+    class_weights = len(idx_train) / np.bincount(y_classif[idx_train])
+
+    class_weights[0] = 2* class_weights[0]
+  
+    train_weights = [class_weights[i] for i in y_classif[idx_train]]
+    model = XGBClassifier()
+    # print("input data:" , X.shape, X[idx_train].reshape(len(idx_train), -1).shape, y_classif[idx_train].shape)
+    model.fit(X[idx_train].reshape(len(idx_train), -1), y_classif[idx_train],
+    sample_weight=train_weights);
+
+    preds = model.predict(X[idx_test].reshape(len(idx_test), -1))
+    true = y_classif[idx_test]
+
+    f1 = f1_score(true, preds)
+    cm = confusion_matrix(true, preds)
+    # print('F1: ', f1)
+    # print('Confusion Matrix: \n', cm)
+    return model, f1, cm
+
+
+
+def regression(X, y_reg, y_classif, random_state=1):
+    idx_train, idx_test, Y_train, Y_test = train_test_split(np.arange(len(y_classif)), 
+    y_classif, test_size=0.3, random_state=random_state, stratify=y_classif)
+    # print(Counter(Y_train), Counter(Y_test), len(idx_train), len(idx_test))
+    # print("input data:" , X.shape, X[idx_train].reshape(len(idx_train), -1).shape, y_reg[idx_train].shape)
+    # Create a DMatrix for more efficiency
+    dtrain = xgb.DMatrix(X[idx_train].reshape(len(idx_train), -1), label=y_reg[idx_train])
+    dtest = xgb.DMatrix(X[idx_test].reshape(len(idx_test), -1), label=y_reg[idx_test])
+
+    # Define the parameters
+    param = {
+        'max_depth': 3,  # the maximum depth of each tree
+        'eta': 0.3,  # the training step for each iteration
+        'objective': 'reg:squarederror',  # error function for regression
+        'eval_metric': 'rmse'  # evaluation metric for regression
+    }
+    num_round = 10  # the number of training iterations
+
+    # Train the model
+    bst = xgb.train(param, dtrain, num_round)
+    preds = bst.predict(dtest)
+    true = y_reg[idx_test]
+    mae = mean_absolute_error(true, preds)
+    return bst, mae
+
+
+
+def prepare_dataset(df, cols, window_size,  prediction_horizon,
+                     autocorr_window = 100, incident_horizon = 50000):
+    X = []
+    y_reg = []
+    incident_ref = []
+    idx_ref = []
+
+    # exclude idx to ensure we have non overlapping sliding windows across failures (e.g. addind samples from failure k-1 to failure k)
+    exclude_idx = []
+    last_samples = df.groupby("incident_nb").agg({"idx":"min"}).values.reshape(-1)
+    for _ in range(window_size - 1):
+        exclude_idx.append(last_samples)
+        last_samples = last_samples+1
+    if len(exclude_idx)>0:
+        exclude_idx = np.concatenate(exclude_idx)
+
+
+    idx = df[(df["survival"] <= incident_horizon) & 
+             (df["survival"]%window_size ==0) &
+             (df["survival"]%autocorr_window ==0) &  
+             (~df["idx"].isin(exclude_idx))]["idx"].values
+    for window in range(window_size):
+        df_= df[df["idx"].isin(idx)].sort_values(by = "idx", ascending = True)
+        X.append(df_[cols].values)
+        if window == 0:
+            y_reg = df_["survival"].values
+            incident_ref = df_["incident_nb"].values
+            idx_ref = df_["idx"].values
+        idx = idx -1
+
+    df["y_temp"] = (df["survival"]<prediction_horizon).astype(int)
+
+    X = np.array(X).transpose(1,2,0)
+    y_reg = np.array(y_reg)
+    incident_ref = np.array(incident_ref)
+    idx_ref = np.array(idx_ref)
+    y_classif = df.iloc[idx_ref]["y_temp"].values
+    return X, y_reg, y_classif, incident_ref, idx_ref
